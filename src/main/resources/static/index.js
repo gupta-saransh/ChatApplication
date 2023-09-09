c9d0ff17ebef2ade869f3a6c57ad210ac5788100
gupta-saransh <saransh287@gmail.com>
2023-09-09 23:34:16 +0530

    Add support for showing currently active users

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,7 @@
 userName = "RandomUser"
 const signInButton = document.getElementById("signInBtn");
+let startUp=true;
+let serverError = false;
 signInButton.addEventListener("click", function sendMessage(){
 
     userName = document.getElementById("nameInput").value;
@@ -11,15 +13,7 @@ signInButton.addEventListener("click", function sendMessage(){
     }
 
     console.log(userName);
-    document.querySelector("#mainChat").style.removeProperty("display");
-    let divToHide = document.getElementsByClassName("signup-container");
-
-    if(divToHide){
-        divToHide[0].style.display = 'none'
-    }
-
     createWebSocketConn();
-
 });
 
 var inputField = document.getElementById("nameInput");
@@ -48,20 +42,32 @@ function createWebSocketConn(){
     
       if(ws!=null)
       {
-        document.getElementById("chatArea").textContent = "Connected to server";
+
+        //document.getElementById("chatArea").textContent = "Connected to server";
         ws.onmessage = message => {
 
-            let existingMessages = document.getElementById("chatArea").value;
-            let newMessage = "";
-            console.log("Existing" + existingMessages);
-            console.log("Incoming" + message.data);
-            newMessage = existingMessages + "\n"+ message.data;
-            console.log("New" + newMessage);
-            document.getElementById("chatArea").textContent = newMessage;
-            let textarea = document.getElementById('chatArea');
-            textarea.scrollTop = textarea.scrollHeight;
+            handleReplyFromServer(message);
         }
+
+        ws.onclose = (event) => {
+            raiseServerErrAlert();
+        };
+
+        ws.onerror = (event) => {
+            raiseServerErrAlert();
+        };
       }
+}
+
+function getCurrTime(){
+
+    let currDate = new Date(); // for now
+    let currHour = currDate.getHours(); // => 9
+    let currMin = currDate.getMinutes(); // =>  30
+
+    let currTime = currHour + ':' + currMin;
+
+    return currTime;
 }
 
 
@@ -90,6 +96,65 @@ function sendMessageToServer(){
     {
         return;
     }
+    console.log("serverError:" + serverError);
+    if(serverError==true)
+    {
+        console.log("Server Error")
+        raiseServerErrAlert();
+    }
     ws.send(inputMessage);
     document.getElementById("msgInput").value = "";
+}
+
+function handleReplyFromServer(serverReply)
+{
+    console.log("EXISITING AREA: " + document.getElementById("chatArea").value +"\n");
+    let serverReplyJsonObj = JSON.parse(serverReply.data);
+    let incomingMessage = serverReplyJsonObj.MESSAGE;
+    let userName = serverReplyJsonObj.USERNAME;
+    let userList = serverReplyJsonObj.UserList === undefined ? "" : serverReplyJsonObj.UserList;
+    switch(serverReplyJsonObj.MESSAGE_TYPE) {
+        case "SYS_MESSAGE":
+            if(startUp)
+            {
+                document.querySelector("#mainChat").style.removeProperty("display");
+                let divToHide = document.getElementsByClassName("signup-container");
+                if(divToHide){
+                    divToHide[0].style.display = 'none'
+                }
+                document.getElementById("chatArea").textContent = "Connected to server";
+                startUp=false;
+            }
+            document.getElementById("userList").innerHTML = "Current Active Users: [" + userList +"]";
+            appendMessageToTextArea(incomingMessage);
+            break;
+        case "CHAT_MESSAGE":
+            appendMessageToTextArea(userName+": "+incomingMessage);
+          break;
+        case "ERROR_MESSAGE":
+            alert(incomingMessage);
+            window.location.reload();
+        break;
+        default:
+          // code block
+      }
+}
+
+function appendMessageToTextArea(incomingMessage){
+    let existingMessages = document.getElementById("chatArea").value;
+
+    let newMessage = "";
+    console.log("Existing" + existingMessages);
+    console.log("Incoming" + incomingMessage);
+    newMessage = existingMessages + "\n" + incomingMessage;
+    console.log("New" + newMessage);
+    document.getElementById("chatArea").textContent = newMessage;
+    let textarea = document.getElementById('chatArea');
+    textarea.scrollTop = textarea.scrollHeight; 
+}
+
+function raiseServerErrAlert()
+{
+    serverError = true;
+    alert("Server Error, please reload the page!");
 }
